9891b1c684873d8fce66b48dd20862be4a944b22
salomondush <63796975+salomondush@users.noreply.github.com>
2022-10-29 20:01:46 -0500

    fixed route export bug

--- express-auth-template/routes/dataRoute.js
+++ express-auth-template/routes/dataRoute.js
@@ -2,5 +2,7 @@ const express = require("express");
 const dataRoute = express.Router(); // Creates a new router object
 
 dataRoute.get("/", (req, res) => {
-    res.send("Data Route!");
+    res.send(" \"93% of people don't check facts they read on the internet\" - Abraham Lincoln");
 });
+
+module.exports = dataRoute;
