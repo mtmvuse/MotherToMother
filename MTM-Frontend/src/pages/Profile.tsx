a0450192c241425e5d23962d79d3780385444c5e
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2023-11-07 14:12:59 -0600

    updated profile page styling

--- MTM-Frontend/src/pages/Profile.tsx
+++ MTM-Frontend/src/pages/Profile.tsx
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../AuthContext";
-import { Typography, CircularProgress } from "@mui/material";
-import './Profile.css';
+import { CircularProgress, Typography } from "@mui/material";
+import "./Profile.css";
 import editButton from "./assets/edit_button.png";
 
 type User = {
@@ -34,12 +34,18 @@ const Profile: React.FC = () => {
     <div className={"profile-container"}>
       <div className={"profile-heading"}>
         <div className={"name-container"}>
-          <Typography className={"heading"} >{user?.displayName}</Typography>
+          <Typography className={"heading"}>{user?.displayName}</Typography>
           <button>
-            <img className ="name-container-button" src={editButton}  alt="Image1"/>
+            <img
+              className="name-container-button"
+              src={editButton}
+              alt="Image1"
+            />
           </button>
         </div>
-        <Typography className={"subheading"} >Organization/ Affiliation</Typography>
+        <Typography className={"subheading"}>
+          Organization / Affiliation
+        </Typography>
       </div>
       {isLoading ? (
         <CircularProgress />
@@ -47,7 +53,7 @@ const Profile: React.FC = () => {
         <div className={"profile-info"}>
           <div className={"inline"}>
             <strong>Phone:</strong>
-            <p className={"value"}>720-520-3566</p>
+            <p className={"value"}>xxx-xxx-xxx</p>
           </div>
           <div className={"inline"}>
             <strong>Email:</strong>
@@ -55,7 +61,9 @@ const Profile: React.FC = () => {
           </div>
           <div className={"inline"}>
             <strong>Address:</strong>
-            <p className={"value wrap"}>478 Allied DriveSuite 104 & 105Nashville, TN 37211</p>
+            <p className={"value wrap"}>
+              478 Allied DriveSuite 104 & 105Nashville, TN 37211
+            </p>
           </div>
         </div>
       )}
@@ -65,7 +73,7 @@ const Profile: React.FC = () => {
         </button>
       </div>
     </div>
-);
+  );
 };
 
 export default Profile;
