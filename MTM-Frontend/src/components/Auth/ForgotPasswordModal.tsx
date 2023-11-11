ad88d8243c14e960cc8b6f09a866af11bc21d700
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2023-11-11 13:46:37 -0600

    added reset instructions modal

---
+++ MTM-Frontend/src/components/Auth/ForgotPasswordModal.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Modal } from "@mui/material";
+import "./ForgotPasswordModal.css";
+
+type propTypes = {
+    open: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+};
+
+const ForgotPasswordModal: React.FC<propTypes> = ({ open, onClose, children }) => {
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+        >
+            <div className="modal">
+                {children}
+            </div>
+        </Modal>
+    );
+};
+
+export default ForgotPasswordModal;
