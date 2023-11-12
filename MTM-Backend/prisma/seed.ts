ddbdb5c9d8a1721f4523a131b96344263c242e4e
Kaleab-A <kaleabasfaw2010@gmail.com>
2023-11-12 14:11:28 -0600

    Fixing indetation

--- MTM-Backend/prisma/seed.ts
+++ MTM-Backend/prisma/seed.ts
@@ -2,7 +2,11 @@
  * feed the database with test data for development
  */
 import type { UserInput } from "../src/types/user";
-import type { OutgoingDonationType, ItemType, DonationDetailsType } from "../src/types/donation";
+import type {
+  OutgoingDonationType,
+  ItemType,
+  DonationDetailsType,
+} from "../src/types/donation";
 import { hashPassword } from "../src/routes/v1/user/user.service";
 import { db } from "../src/utils/db.server";
 
@@ -30,69 +34,69 @@ const getUsers = (): Array<UserInput> => {
 };
 
 const getOutgoingDonations = (): Array<OutgoingDonationType> => {
-    return [
-        {
-            id: 1,
-            requesterID: 5, 
-            date: new Date('2023-01-01'),
-            numberServed: 100,
-            whiteNum: 25,
-            latinoNum: 25,
-            blackNum: 25,
-            nativeNum: 10,
-            asianNum: 10,
-            otherNum: 5,
-        },
-        {
-            id: 2,
-            requesterID: 6,
-            date: new Date('2023-02-01'),
-            numberServed: 150,
-            whiteNum: 30,
-            latinoNum: 30,
-            blackNum: 30,
-            nativeNum: 20,
-            asianNum: 20,
-            otherNum: 20,
-        },
-    ];
-}
+  return [
+    {
+      id: 1,
+      requesterID: 5,
+      date: new Date("2023-01-01"),
+      numberServed: 100,
+      whiteNum: 25,
+      latinoNum: 25,
+      blackNum: 25,
+      nativeNum: 10,
+      asianNum: 10,
+      otherNum: 5,
+    },
+    {
+      id: 2,
+      requesterID: 6,
+      date: new Date("2023-02-01"),
+      numberServed: 150,
+      whiteNum: 30,
+      latinoNum: 30,
+      blackNum: 30,
+      nativeNum: 20,
+      asianNum: 20,
+      otherNum: 20,
+    },
+  ];
+};
 
 const getItem = (): Array<ItemType> => {
-    return [
-        {
-            id: 1,
-            name: "Apples",
-        },
-        {
-            id: 2,
-            name: "Oranges",
-        }
-    ]
-}
+  return [
+    {
+      id: 1,
+      name: "Apples",
+    },
+    {
+      id: 2,
+      name: "Oranges",
+    },
+  ];
+};
 
 const getDonationDetails = (): Array<DonationDetailsType> => {
-    return [
-        {
-            itemID: 1,                
-            quantityOld: 100,         
-            quantityNew: 150,         
-            outgoingDonationId: 1,    
-        },
-        {
-            itemID: 2,
-            quantityOld: 150,
-            quantityNew: 200,
-            outgoingDonationId: 2,
-        }
-    ]
-}
+  return [
+    {
+      itemID: 1,
+      quantityOld: 100,
+      quantityNew: 150,
+      outgoingDonationId: 1,
+    },
+    {
+      itemID: 2,
+      quantityOld: 150,
+      quantityNew: 200,
+      outgoingDonationId: 2,
+    },
+  ];
+};
 
 async function clearData() {
   await db.user.deleteMany({});
   await db.outgoingDonation.deleteMany({});
   await db.item.deleteMany({});
-  await db.donationDetails.deleteMany({}); 
+  await db.donationDetails.deleteMany({});
 }
 
 const seed = async () => {
@@ -109,29 +113,28 @@ const seed = async () => {
     await db.user.create({ data: { ...userInfo, hash: hash, salt: salt } });
   }
 
-    const outgoingDonations = getOutgoingDonations();
-    console.log("Seeding outgoingDonations...");
+  const outgoingDonations = getOutgoingDonations();
+  console.log("Seeding outgoingDonations...");
 
-    for (const outgoingDonation of outgoingDonations) {
-        await db.outgoingDonation.create({ data: outgoingDonation });
-    }
+  for (const outgoingDonation of outgoingDonations) {
+    await db.outgoingDonation.create({ data: outgoingDonation });
+  }
 
-    const items = getItem();
-    console.log("Seeding items...");
+  const items = getItem();
+  console.log("Seeding items...");
 
-    for (const item of items) {
-        await db.item.create({ data: item });
-    }
+  for (const item of items) {
+    await db.item.create({ data: item });
+  }
 
-    const donationDetails = getDonationDetails();
-    console.log("Seeding donationDetails...");
+  const donationDetails = getDonationDetails();
+  console.log("Seeding donationDetails...");
 
-    for (const donationDetail of donationDetails) {
-        await db.donationDetails.create({ data: donationDetail });
-    }
+  for (const donationDetail of donationDetails) {
+    await db.donationDetails.create({ data: donationDetail });
+  }
 
-    console.log("Seeding complete!");
+  console.log("Seeding complete!");
 };
 
 seed();
-
