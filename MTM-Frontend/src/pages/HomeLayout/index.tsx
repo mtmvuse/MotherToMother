13336fe7cdf82646e09380077c8b27bee7966459
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2023-12-05 00:42:17 -0600

    cleanup

--- MTM-Frontend/src/pages/HomeLayout/index.tsx
+++ MTM-Frontend/src/pages/HomeLayout/index.tsx
@@ -1,35 +1,12 @@
-import React, { useState, useEffect } from "react";
 import styles from "./index.module.css";
-import { Link } from "react-router-dom";
-import { useAuth } from "../../AuthContext";
 import { Outlet, useLocation } from "react-router-dom";
-import { Container, Box, Grid } from "@mui/material";
-import { constructPayloadHeaders } from "../../lib/utils";
+import { Container } from "@mui/material";
 import Navbar from "../../components/NavigationBar/BottomNavbar";
 import { TopBar } from "../../components/NavigationBar/TopBar";
 
 export const HomeLayout: React.FC = () => {
-  const [fact, setFact] = useState<string>("");
-  const { currentUser } = useAuth();
   const location = useLocation();
   const isHomeIndex = location.pathname === "/home";
-
-  useEffect(() => {
-    const fetchFact = async () => {
-      try {
-        const token = await currentUser?.getIdToken();
-        const res = await fetch(
-          "/api/example/",
-          constructPayloadHeaders(token),
-        );
-        setFact(await res.text());
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    void fetchFact();
-  }, [currentUser]);
 
   return (
     <Container className={styles.container} sx={isHomeIndex ? { px: 0 } : {}}>
@@ -41,5 +18,3 @@ export const HomeLayout: React.FC = () => {
     </Container>
   );
 };
-
-// font for return click function for return fix top bar
