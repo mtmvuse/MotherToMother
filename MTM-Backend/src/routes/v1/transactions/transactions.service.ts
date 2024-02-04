3ca68926be5e8dce958817b23ce3c8177669b9d5
SySyAli <syedansabali786@gmail.com>
2024-02-04 15:26:44 -0600

    Added additional Information to the Transactions API

--- MTM-Backend/src/routes/v1/transactions/transactions.service.ts
+++ MTM-Backend/src/routes/v1/transactions/transactions.service.ts
@@ -16,37 +16,39 @@ export const getTransactions = async () => {
     },
   });
 
-  const transformedData = donations.map((transaction) => {
-    const details = transaction.DonationDetail.reduce(
-      (
-        acc: {
-          amount: number;
-          items: { product: string; category: string; status: string }[];
-        },
-        detail,
-      ) => {
-        acc.amount +=
-          detail.item.valueUsed * detail.usedQuantity +
-          detail.item.valueNew * detail.newQuantity;
-        acc.items.push({
-          product: detail.item.name,
-          category: detail.item.category,
-          status: detail.usedQuantity > 0 ? "Used" : "New",
-        });
-        return acc;
-      },
-      { amount: 0, items: [] },
-    );
+  const transformedData = donations.map((donation) => {
+    const details = donation.DonationDetail.map((detail) => ({
+      item: detail.item.name,
+      status: detail.usedQuantity > 0 ? "Used" : "New",
+      value:
+        detail.usedQuantity > 0 ? detail.item.valueUsed : detail.item.valueNew,
+      quantity:
+        detail.usedQuantity > 0 ? detail.usedQuantity : detail.newQuantity,
+      total:
+        detail.usedQuantity * detail.item.valueUsed +
+        detail.newQuantity * detail.item.valueNew,
+    }));
+
+    const total = details.reduce((acc, detail) => acc + detail.total, 0);
 
     return {
-      orderId: transaction.id,
-      items: details.items,
-      organization: transaction.user.Organization?.name || "Individual",
-      amount: details.amount,
+      id: donation.id,
+      date: donation.date,
+      organization: donation.user.Organization?.name || "Individual",
+      total: total,
+      items: details.length,
+      type: donation.user.Organization
+        ? donation.user.Organization.type
+        : "Individual",
+      details: details,
     };
   });
+
   return transformedData;
 };
 
 // TODO: add date, type - incoming (public, corporate) outgoing (to the agency - look at organization type)
 // [order_id, product, category, organization, amount, date, status]
+// look at the updated body
+// TOOD: req. queries: page, pageSize.
+// response body: [{id, date, organization, total, items, type, details: [{item, status, value, quantity, total}]]
