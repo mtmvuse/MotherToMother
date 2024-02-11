0d6f4763fcf2a7226f1372454128ee72c752a91a
SySyAli <syedansabali786@gmail.com>
2024-02-11 00:02:42 -0600

    Creating Incoming Donation POST Service Function

--- MTM-Backend/src/routes/v1/donation/donation.router.ts
+++ MTM-Backend/src/routes/v1/donation/donation.router.ts
@@ -139,13 +139,29 @@ donationRouter.post("/v1/outgoing", createOutgoingDonation);
 donationRouter.post(
   "/v1/incoming",
   async (req: Request, res: Response, next: NextFunction) => {
-    const donationReqBody = req.body as IncomingDonationRequestBodyType;
+    try {
+      const donationReqBody = req.body as IncomingDonationRequestBodyType;
+      if (!donationReqBody.userId) {
+        return res.status(400).json({
+          error: "User ID must be entered",
+        });
+      }
+      // make sure the user exists by checking the id in the database
+      const createdDonation =
+        await DonationService.createIncomingDonation(donationReqBody);
 
-    await DonationService.createIncomingDonation(donationReqBody);
-
-    return res.status(200).json({
-      message: "Incoming donation created successfully",
-    });
+      if (createdDonation) {
+        return res.status(200).json({
+          createdDonation,
+        });
+      } else {
+        return res.status(400).json({
+          error: "User does not exist",
+        });
+      }
+    } catch (e) {
+      next(e);
+    }
   },
 );
 
