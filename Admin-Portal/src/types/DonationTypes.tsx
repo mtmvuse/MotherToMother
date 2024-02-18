b62cda48ab075f5c599e3672c489ed6d8e2a135b
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-02-18 03:23:02 -0600

    creating add donation type

--- Admin-Portal/src/types/DonationTypes.tsx
+++ Admin-Portal/src/types/DonationTypes.tsx
@@ -1,13 +1,29 @@
 export interface itemTypes {
-    id: number;
-    item: string;
-    status: string;
-    value: number;
-    quantity: number;
+  id: number;
+  item: string;
+  status: string;
+  value: number;
+  quantity: number;
 }
 
 export interface demographicTypes {
-    id: number;
-    kidGroup: string;
-    quantity: number;
+  id: number;
+  kidGroup: string;
+  quantity: number;
+}
+
+export interface AddOutgoingDonationType {
+  userId: number;
+  donationDetails: {
+    itemId: number;
+    usedQuantity: number;
+    newQuantity: number;
+  }[];
+  numberServed: number;
+  whiteNum: number;
+  latinoNum: number;
+  blackNum: number;
+  nativeNum: number;
+  asianNum: number;
+  otherNum: number;
 }
