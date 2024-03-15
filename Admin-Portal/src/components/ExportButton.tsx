2b5ad0c6ae217b01560f626c2649979f335e7a95
Joseph <joeyspec0715@gmail.com>
2024-03-15 08:31:47 -0500

    added styling to AP

--- Admin-Portal/src/components/ExportButton.tsx
+++ Admin-Portal/src/components/ExportButton.tsx
@@ -1,22 +1,18 @@
-import React from "react";
-import { Button } from "@mui/material";
-import DownloadIcon from "@mui/icons-material/Download";
+import React from 'react';
+import { Button, Typography } from '@mui/material';
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
 
 interface ExportButtonProps {
-  handleExport: () => void;
+	handleExport: () => void;
 }
 
 const ExportButton: React.FC<ExportButtonProps> = ({ handleExport }) => {
-  return (
-    <Button
-      onClick={handleExport}
-      variant="contained"
-      sx={{ margin: "auto 10px 10px auto" }}
-    >
-      <DownloadIcon sx={{ marginRight: "6px" }} />
-      Export
-    </Button>
-  );
+	return (
+		<Button onClick={handleExport} variant='contained' sx={{ margin: 'auto 10px 10px auto' }}>
+			<DownloadOutlinedIcon sx={{ marginRight: '6px' }} />
+			<Typography sx={{ textTransform: 'capitalize', fontSize: '18px' }}>Export</Typography>
+		</Button>
+	);
 };
 
 export default ExportButton;
--- Admin-Portal/src/pages/HomeLayout.tsx
+++ Admin-Portal/src/pages/HomeLayout.tsx
@@ -1,124 +1,139 @@
-import React from "react";
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import { Box, Tab, Tabs, Typography, Button } from "@mui/material";
-import { CAPTIONS } from "../lib/constants";
-import { useAuth } from "../lib/contexts";
-import "./styles/datagrid.css";
-import "./styles/sidebar.css";
-import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
-import ContentPasteSearchIcon from "@mui/icons-material/ContentPasteSearch";
-import GroupIcon from "@mui/icons-material/Group";
-import CorporateFareIcon from "@mui/icons-material/CorporateFare";
-import LocalAtmIcon from "@mui/icons-material/LocalAtm";
-import AssessmentIcon from "@mui/icons-material/Assessment";
-import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import React from 'react';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { Box, Tab, Tabs, Typography, Button, Avatar } from '@mui/material';
+import { CAPTIONS } from '../lib/constants';
+import { useAuth } from '../lib/contexts';
+import './styles/datagrid.css';
+import './styles/sidebar.css';
+import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
+import ContentPasteSearchOutlinedIcon from '@mui/icons-material/ContentPasteSearchOutlined';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
+import CorporateFareOutlinedIcon from '@mui/icons-material/CorporateFareOutlined';
+import LocalAtmOutlinedIcon from '@mui/icons-material/LocalAtmOutlined';
+import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SearchIcon from '@mui/icons-material/Search';
+import favicon from '../assets/favicon.ico';
 
 interface LinkTabProps {
-  label: string;
-  href?: string;
-  selected?: boolean;
+	label: string;
+	href?: string;
+	selected?: boolean;
 }
-
 const iconMap: { [key: string]: React.ElementType } = {
-  donations: VolunteerActivismIcon,
-  inventory: ContentPasteSearchIcon,
-  users: GroupIcon,
-  admins: CorporateFareIcon,
-  cashdonations: LocalAtmIcon,
-  reports: AssessmentIcon,
+	donations: VolunteerActivismOutlinedIcon,
+	inventory: ContentPasteSearchOutlinedIcon,
+	users: GroupOutlinedIcon,
+	administration: CorporateFareOutlinedIcon,
+	cashdonations: LocalAtmOutlinedIcon,
+	reports: AssessmentOutlinedIcon,
 };
-const LinkTab = ({ label, ...props }: LinkTabProps) => {
-  const navigate = useNavigate();
-  const IconComponent = iconMap[label.toLowerCase().replace(/\s/g, "")];
 
-  return (
-    <Tab
-      className="navTab"
-      iconPosition="start"
-      icon={IconComponent && <IconComponent />}
-      component="a"
-      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        event.preventDefault();
-        navigate(props.href!);
-      }}
-      aria-current={props.selected && "page"}
-      sx={{ "&.Mui-selected": { bgcolor: "#bbdefb" } }}
-      label={
-        <Typography align={"left"} className="navTabText">
-          {label}
-        </Typography>
-      }
-      {...props}
-    />
-  );
+const LinkTab = ({ label, ...props }: LinkTabProps) => {
+	const navigate = useNavigate();
+	const IconComponent = iconMap[label.toLowerCase().replace(/\s/g, '')];
+
+	return (
+		<Tab
+			className='navTab'
+			iconPosition='start'
+			icon={IconComponent && <IconComponent />}
+			component='a'
+			onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+				event.preventDefault();
+				navigate(props.href!);
+			}}
+			aria-current={props.selected && 'page'}
+			sx={{
+				'&.Mui-selected .navTabText': { color: '#1976d2' },
+				'&.Mui-selected': { bgcolor: '#e3f2fd' },
+			}}
+			label={
+				<Typography align={'left'} className='navTabText'>
+					{label}
+				</Typography>
+			}
+			{...props}
+		/>
+	);
 };
 
 const HomeLayout: React.FC = () => {
-  const location = useLocation();
-  const currentPathname = location.pathname.substring(1);
-  const captionsToPathnames = CAPTIONS.map((caption) =>
-    caption.replace(/\s/g, "").toLowerCase()
-  );
-  const [curPage, setCurPage] = React.useState(
-    captionsToPathnames.indexOf(currentPathname.toLowerCase())
-  );
-  const { logout } = useAuth();
-  const navigate = useNavigate();
+	const location = useLocation();
+	const currentPathname = location.pathname.substring(1);
+	const captionsToPathnames = CAPTIONS.map((caption) => caption.replace(/\s/g, '').toLowerCase());
+	const [curPage, setCurPage] = React.useState(captionsToPathnames.indexOf(currentPathname.toLowerCase()));
+	const { logout } = useAuth();
+	const navigate = useNavigate();
 
-  const handleChange = (_event: React.SyntheticEvent, newPage: number) => {
-    setCurPage(newPage);
-  };
-  const handleLogout = async () => {
-    await logout();
-    navigate("/login");
-  };
+	const handleChange = (_event: React.SyntheticEvent, newPage: number) => {
+		setCurPage(newPage);
+	};
+	const handleLogout = async () => {
+		await logout();
+		navigate('/login');
+	};
 
-  return (
-    <StyledEngineProvider injectFirst>
-      <Box sx={{ display: "flex" }} className="sideBar">
-        <Box
-          sx={{
-            flex: "0 0 15%",
-            borderRight: 1,
-            borderColor: "divider",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Tabs
-            value={curPage}
-            onChange={handleChange}
-            aria-label="nav tabs"
-            role="navigation"
-            orientation="vertical"
-            className="tabsContainer"
-          >
-            {CAPTIONS.map((caption, index) => (
-              <LinkTab
-                key={index}
-                label={caption}
-                href={`/${caption.replace(/\s/g, "")}`}
-              />
-            ))}
-          </Tabs>
-          <Button sx={{ marginButton: "auto" }} onClick={handleLogout}>
-            Logout
-          </Button>
-          <Box className="appInfoContainer">
-            <Typography className="appNameText">Mother to Mother</Typography>
-            <Typography className="versionText">Version 1.0.0</Typography>
-          </Box>
-        </Box>
-        <Box sx={{ flex: 1, p: 3 }}>
-          <Typography variant="h5" gutterBottom sx={{ marginBottom: "25px" }}>
-            {CAPTIONS[curPage]}
-          </Typography>
-          {<Outlet />}
-        </Box>
-      </Box>
-    </StyledEngineProvider>
-  );
+	return (
+		<StyledEngineProvider injectFirst>
+			<Box className='root-container'>
+				<Box className='topbar-container'>
+					<Box className='topbar-profile'>
+						<Avatar src={favicon} sx={{ borderRadius: 0 }} alt='Favicon' />
+						<Typography variant='h5' className='text'>
+							Name
+						</Typography>
+						<ExpandMoreIcon />
+					</Box>
+
+					<Box className='topbar-search'>
+						<SearchIcon />
+						<Typography>Quick search</Typography>
+					</Box>
+				</Box>
+
+				<Box sx={{ display: 'flex' }}>
+					<Box
+						sx={{
+							flex: '0 0 15%',
+							borderRight: 1,
+							borderColor: 'divider',
+							height: '100vh',
+							display: 'flex',
+							flexDirection: 'column',
+						}}
+					>
+						<Tabs
+							value={curPage}
+							onChange={handleChange}
+							aria-label='nav tabs'
+							role='navigation'
+							orientation='vertical'
+							className='tabsContainer'
+						>
+							{CAPTIONS.map((caption, index) => (
+								<LinkTab key={index} label={caption} href={`/${caption.replace(/\s/g, '')}`} />
+							))}
+						</Tabs>
+						<Button sx={{ marginButton: 'auto' }} onClick={handleLogout}>
+							Logout
+						</Button>
+						<Box className='appInfoContainer'>
+							<Typography className='appNameText'>Mother to Mother</Typography>
+							<Typography className='versionText'>Version 1.0.0</Typography>
+						</Box>
+					</Box>
+					<Box sx={{ flex: 1, p: 3 }} className='page-container'>
+						<Typography variant='h4' className='page-title'>
+							{CAPTIONS[curPage]}
+						</Typography>
+						{<Outlet />}
+					</Box>
+				</Box>
+			</Box>
+		</StyledEngineProvider>
+	);
 };
 
 export default HomeLayout;
