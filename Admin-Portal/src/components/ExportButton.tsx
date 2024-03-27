47ca45f78ea5f1d12df04af6b9b3850f942aa9fd
Joseph <joeyspec0715@gmail.com>
2024-03-26 21:09:32 -0500

    fix export styling class name

--- Admin-Portal/src/components/ExportButton.tsx
+++ Admin-Portal/src/components/ExportButton.tsx
@@ -1,24 +1,23 @@
-import React from "react";
-import { Button, Typography } from "@mui/material";
-import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import React from 'react';
+import { Button, Typography } from '@mui/material';
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
 
 interface ExportButtonProps {
-  handleExport: () => void;
+	handleExport: () => void;
 }
 
 const ExportButton: React.FC<ExportButtonProps> = ({ handleExport }) => {
-  return (
-    <Button
-      onClick={handleExport}
-      variant="contained"
-      sx={{ margin: "auto 10px 10px auto" }}
-    >
-      <DownloadOutlinedIcon sx={{ marginRight: "6px" }} />
-      <Typography sx={{ textTransform: "capitalize", fontSize: "18px" }}>
-        Export
-      </Typography>
-    </Button>
-  );
+	return (
+		<Button
+			onClick={handleExport}
+			variant='contained'
+			sx={{ margin: 'auto 10px 10px auto' }}
+			className='table-add-export-button'
+		>
+			<DownloadOutlinedIcon sx={{ marginRight: '6px' }} />
+			<Typography sx={{ textTransform: 'capitalize', fontSize: '18px' }}>Export</Typography>
+		</Button>
+	);
 };
 
 export default ExportButton;
