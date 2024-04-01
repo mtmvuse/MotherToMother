d68a728a0cf45850fe28a96224c2e57f4ea6df55
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-03-31 23:33:51 -0500

    updating donate accordian

--- MTM-Frontend/src/components/Accordions/DonateAccordion.tsx
+++ MTM-Frontend/src/components/Accordions/DonateAccordion.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import { Typography, Button } from "@mui/material";
 
 import { Accordion } from "./Accordion";
 import { AccordionSummary } from "./AccordionSummary";
@@ -30,14 +30,38 @@ const DonateAccordion: React.FC<DonateAccordionProps> = ({
         >
           <Typography className="square-button">Donate</Typography>
         </AccordionSummary>
-        <AccordionDetails customStyles={{ backgroundColor: "#EBF6E9" }}>
+        <AccordionDetails
+          customStyles={{
+            backgroundColor: "#EBF6E9",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
           <Typography>
-            For almost two decades, Nashville-based Mother To Mother has been
-            working to ensure children in our community have access to three
-            basic essentials that often slip through the cracks of government
-            subsidies and charitable support:​ diapers car seats a safe place to
-            sleep
+            Join Mother To Mother's mission to help families thrive by providing
+            car seats, diapers, cribs, and other essential resources so that
+            under-resourced children in our community can be safe and healthy.
           </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            href="https://buy.stripe.com/3csbLc7SE2Z3bxm3cc"
+            style={{
+              marginTop: "15px",
+              marginBottom: "15px",
+              backgroundColor: "var(--mtmNavy)",
+              color: "white",
+              height: "32px",
+              width: "150px",
+              fontFamily: "Inter, sans-serif",
+              fontSize: "15px",
+              fontWeight: "800",
+            }}
+          >
+            Donate Here
+          </Button>
         </AccordionDetails>
       </Accordion>
     </>
